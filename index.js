7dfb5489a08b064e302878f326ae35f0747b4240
Ashly Roy <ashlyroy10@gmail.com>
2023-03-07 14:11:29 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,104 @@
+//Basic Server Structure
+
+//importing DB file to server file
+const CinemaInfo = require('./model/movieDb');
+
+//for hosting
+const path = require('path');
+
+//1.Import Express
+const express = require("express");
+
+//2.Initialising Express
+const app = new express();
+
+//Parsing Body Parameter for POST function
+app.use(express.urlencoded({extended : true}));
+app.use(express.json());
+
+//added for hosting
+app.use(express.static(path.join(__dirname,'/build')));
+
+
+//CORS Policy - Port Connection
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type ");
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+//3.API Creation
+
+//Add Movie
+app.post('/api/movie/create',(req,res)=>{
+    try{
+        console.log(req.body);//data is in server
+        let cinema = new CinemaInfo(req.body);//passing to database
+        cinema.save();//data saved to db
+        res.send("Data Added to db");
+    }
+   catch(error){
+        res.status(500).send(error);
+   }
+    
+});
+
+//Read Movie
+app.get('/api/movie/read',async (req,res)=>{
+    try{
+        let result = await CinemaInfo.find();
+        res.json(result);
+    }
+    catch(error){
+        res.status(500).send(error);
+    }
+    
+});
+
+//Update Movie
+app.post('/api/movie/update',async (req,res)=>{
+    try{
+        console.log("update",req.body);
+        let result = await CinemaInfo.findByIdAndUpdate(req.body._id,req.body);
+        res.json(result);
+    }
+    catch(error){
+        res.status(500).send(error);
+    }
+    
+});
+
+//Delete Movie
+app.post('/api/movie/delete',async (req,res)=>{
+    try{
+        let result = await CinemaInfo.findByIdAndDelete(req.body._id);
+        res.send("Data Deleted");
+    }
+    catch(error){
+        res.status(500).send(error);
+    }
+    
+});
+
+//Search Movie
+app.post('/api/movie/search',async (req,res)=>{
+    try{
+        //let result = await CinemaInfo.find(req.body);
+        //let result = await CinemaInfo.find({"moviename":{$regex:'.*' + req.body.moviename + '.*'}});
+        let result = await CinemaInfo.find({"moviename":{$regex:new RegExp(req.body.moviename, "i")}});
+        res.json(result);
+    }
+    catch(error){
+        res.status(500).send(error);
+    }
+    
+});
+
+
+//added for hosting
+app.get('/*', function(req, res) {res.sendFile(path.join(__dirname,'/build/index.html')); });
+
+//4.Setting PORT number
+app.listen(7501, ()=> {console.log("Server is running in port 7501")})
